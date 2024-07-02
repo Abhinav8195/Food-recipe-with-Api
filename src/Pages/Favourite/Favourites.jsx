fd9a22d145de5d1d9b25d91e6fc21e743d8b1909
Abhinav8195 <149519597+Abhinav8195@users.noreply.github.com>
2024-07-02 15:08:04 +0530

    Update Favourites.jsx

--- src/Pages/Favourite/Favourites.jsx
+++ src/Pages/Favourite/Favourites.jsx
@@ -3,21 +3,19 @@ import { GlobalContext } from '../../Context'
 import Recipe from '../../Components/Recipe/Recipe'
 
 const Favourites = () => {
-  const { favorites } = useContext(GlobalContext)
+  const {favorites} = useContext(GlobalContext)
+ 
   return (
-    <div className='recipe-container'>
+    <div className='py-8 container mx-auto flex flex-wrap justify-center gap-10'>
       {
-        favorites && favorites.length > 0 ?
-          favorites.map((item) =>
-            <Recipe item={item} key={item.id} />
-          )
-          : <div>
-            <h1 className='no-recipes'>Nothing is added!</h1>
-          </div>
+        favorites && favorites.length>0 ? 
+        favorites.map((item) => 
+          <Recipe item={item}/>
+        )
+        :<div>
+          <h1 className='text-2xl text-center'>Nothing is added !</h1>
+        </div>
       }
-       <div className='designer-name'>
-        Designed by: Abhinav
-      </div>
     </div>
   )
 }
